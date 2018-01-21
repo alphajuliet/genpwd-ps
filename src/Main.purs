96651635eee33c490557c04d2711d036a6a943e5
alphajuliet <andrew@alphajuliet.com>
2018-01-21 11:57:06 +1100

    Create stub web UI using smolder. Implement basic random list element generator.

--- src/Main.purs
+++ src/Main.purs
@@ -4,13 +4,39 @@
 module Main where
 
 import Prelude
+import Data.Array (index, length)
+import Data.Maybe (Maybe(..))
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
--- import Control.Monad.Eff.Random (RANDOM, random, randomInt)
+import Control.Monad.Eff.Console (CONSOLE, logShow)
+import Control.Monad.Eff.Random (RANDOM, randomInt)
 
-main :: Eff (console :: CONSOLE) Unit
+--------
+
+unwrapMaybe :: Maybe String -> String
+unwrapMaybe Nothing = ""
+unwrapMaybe (Just x) = x
+
+randomElement :: ∀ e. Array String -> Eff (random :: RANDOM | e) String
+randomElement arr = unwrapMaybe <$> ranIndex arr
+  where
+    ranIndex :: forall a e. Array a -> Eff (random :: RANDOM | e) (Maybe a)
+    ranIndex xs = map (index xs) (randomInt 0 $ length xs)
+
+f :: ∀ e. Array (Array String) -> Array (Eff (random :: RANDOM | e) String)
+f = map randomElement
+
+consonant :: Array String
+consonant = ["t", "d", "s", "j", "k", "g", "h", "b", "p", "m", "n", "r"]
+
+vowel :: Array String
+vowel = ["a", "i", "e", "u", "o"]
+
+--------
+
+--------
+-- main :: Eff _ Unit
 main = do
-  log "Hello sailor!"
-
+  result <- randomElement ["abc", "def", "ghi"]
+  logShow result
 
 -- The End
